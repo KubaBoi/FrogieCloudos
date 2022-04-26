cd6f853f6997dd4445127956ea5674595535ab2d
KubaBoi <jakubanderle@outlook.cz>
2022-04-26 19:41:42 +0200

    debug

--- src/resources/web/scripts/start.js
+++ src/resources/web/scripts/start.js
@@ -22,21 +22,7 @@ var parentTree = null;
 async function start() {
     var response = await callEndpoint("GET", "/main/init");
     if (response.ERROR == null) {
-        foldersCookies = getCookie("openFolders");
-        if (foldersCookies == "") {
-            openFolder(response.PATH, response.ROOT);
-        }
-        else {
-            foldersCookiesArray = foldersCookies.split(",");
-            for (var i = 0; i < foldersCookiesArray.length; i++) {
-                foldRoot = foldersCookiesArray[i].split("|");
-                openFolder(foldRoot[0], foldRoot[1]);
-            }
-        }
-
-        var gridType = getCookie("gridType");
-        if (gridType == "") setGridStyle("nGrid");
-        else setGridStyle(gridType);
+        openFolder(response.PATH, response.ROOT);
     }
     else {
         showWrongAlert("ERROR", response.ERROR, alertTime);
