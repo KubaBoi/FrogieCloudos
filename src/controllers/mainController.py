2165197a2763bae8f589956ba844153b78900781
KubaBoi <jakubanderle@outlook.cz>
2022-06-19 15:29:36 +0200

    debug

--- src/controllers/mainController.py
+++ src/controllers/mainController.py
@@ -98,7 +98,6 @@ class MainController(cc):
 
 		with open(ResMan.web('images', pth), "wb") as f:
 			Logger.info(f"Saving: {ResMan.web('images', pth)}")
-			print(file)
 			f.write(file)
 
 		return cc.createResponse({"STATUS": "File was uploaded"}, 200)
